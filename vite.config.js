1b58fa62e7612eba5d0884188eccfbd4982f83bc
Achyut <162984049+ACHYUTKRCHAUDHARY@users.noreply.github.com>
2025-08-27 13:07:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ACHYUTKRCHAUDHARY/', // 👈 must match your repo name exactly
+  base: '/ACHYUTKRCHAUDHARY/',
 })
